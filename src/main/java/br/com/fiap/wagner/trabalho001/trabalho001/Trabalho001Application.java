3a878faeed6add895a501b9dbb88a78612b5bb85
Wagner Gonçalves <wagner@ar.com.br>
2021-05-06 21:01:49 -0300

    Commit inicial

---
+++ src/main/java/br/com/fiap/wagner/trabalho001/trabalho001/Trabalho001Application.java
@@ -0,0 +1,13 @@
+package br.com.fiap.wagner.trabalho001.trabalho001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trabalho001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trabalho001Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/wagner/trabalho001/trabalho001/Trabalho001ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.wagner.trabalho001.trabalho001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trabalho001ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
